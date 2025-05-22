ea65c80683f377cf7f6b44bf5b80dc45bde5440e
Bd-Mutant7 <spicezke@gmail.com>
2025-05-22 13:54:02 +0300

    firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHRmtPI-FcsH2VXsbnXFhedv4e5ay-xco",
+  authDomain: "simpzone-c261f.firebaseapp.com",
+  projectId: "simpzone-c261f",
+  storageBucket: "simpzone-c261f.firebasestorage.app",
+  messagingSenderId: "112722316822",
+  appId: "1:112722316822:web:40f25acde13d98821f2f37"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
